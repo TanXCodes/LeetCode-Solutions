d7e511b91d392a7ff4d2ae3e47c8c2259b51911b
Tanay Dubey <tanayd007@gmail.com>
2026-09-08 11:20:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (57.08%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -1,11 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-        int c = 0;
-        for(int i = 1; i <= n; i++){
-            if(i >= 1000){
-                c++;
-            }
+        if(n<1000){
+            return 0;
         }
-        return c;
+        return n - 999;
     }
 }
