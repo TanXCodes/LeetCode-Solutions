fdb0dd6bfbb56ff7bc9b35fb6bb7e95b2ffd9438
Tanay Dubey <tanayd007@gmail.com>
2026-09-08 11:32:24 +0530

    Time: 2 ms (99.31%) | Memory: 44.1 MB (82.97%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,57 +1,19 @@
-class Solution{
-    public boolean isPalindrome(String s){
-        String str = "";
-        for(int i=0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            if(Character.isLetterOrDigit(ch)){
-                str += Character.toLowerCase(ch);
+class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while(l<r){
+            while(l<r && !Character.isLetterOrDigit(s.charAt(l))){
+                l++;
+            } 
+            while(l<r && !Character.isLetterOrDigit(s.charAt(r))){
+                r--;
             }
+            if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))){
+                return false;
+            }
+            l++;
+            r--;
         }
-
-        String rev = new StringBuilder(str).reverse().toString();
-
-        return str.equals(rev);
+        return true;
     }
 }
-
-
-
-
-// class Solution{
-//     public boolean isPalindrome(String s){
-//         String str = "";
-//         for(int i=0; i<s.length(); i++){
-//             char ch = s.charAt(i);
-//             if(Character.isLetterOrDigit(ch)){
-//                 str += Character.toLowerCase(ch);
-//             }
-//         }
-//         String rev = "";
-//         for(int i=str.length() - 1 ; i >= 0; i--){
-//             rev += str.charAt(i);
-//         }
-//         return rev.equals(str);
-//     }
-// }
-
-
-
-
-
-
-// class Solution {
-//     public boolean isPalindrome(String s) {
-//         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
-//         int left = 0;
-//         int right = s.length() - 1;
-
-//         while (left < right) {
-//             if (s.charAt(left) != s.charAt(right)) {
-//                 return false;
-//             }
-//             left++;
-//             right--;
-//         }
-//         return true;        
-//     }
-// }
